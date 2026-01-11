37eed82706720835aeedc6e0acf5bb5489b0ea27
gajeranirjar <gnirjar1234@gmail.com>
2026-01-11 21:40:39 +0530

    fix log

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import { Posts } from "./components/Posts";
 
 const App = () => {
-    console.log("👌 ~ import.meta.env.VITE_API_URL:", import.meta.env.VITE_API_URL)
 
   return (
     <section className="main-section">
